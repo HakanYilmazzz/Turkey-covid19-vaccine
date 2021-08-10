12dcf09d486bc23664665b1e0674ed1053ce081d
Hakan YILMAZ <hakanyilmazzz93@gmail.com>
2021-08-10 23:41:52 +0300

    First Update

--- asi.js
+++ asi.js
@@ -19,7 +19,6 @@ fetch('https://cors.bridged.cc/https://covid19asi.saglik.gov.tr' , {
     fdoz=0;
     sdoz=0;
     tdoz=0;
-    console.log(JsonData)
         for (var i = 1; i < 82; i++) {
             GenelToplam=Number(JsonData[i].toplam.split('.').join(''))+GenelToplam;
             fdoz = Number(JsonData[i].birinciDoz.split('.').join(''))+fdoz;
@@ -36,20 +35,27 @@ fetch('https://cors.bridged.cc/https://covid19asi.saglik.gov.tr' , {
             tr.append("<td>" + ((Number(JsonData[i].toplam.split('.').join('')) - Number(JsonData[i].birinciDoz.split('.').join('')) - Number(JsonData[i].ikinciDoz.split('.').join(''))).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')) + "</td>");
             $('#myTable').append(tr);
         }
+    document.getElementById("toplam").innerText = GenelToplam.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    document.getElementById("fdoz").innerText = fdoz.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    document.getElementById("sdoz").innerText = sdoz.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    document.getElementById("tdoz").innerText = tdoz.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
 
-            document.getElementById("toplam").innerText = GenelToplam.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-            document.getElementById("fdoz").innerText = fdoz.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-            document.getElementById("sdoz").innerText = sdoz.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-            document.getElementById("tdoz").innerText = tdoz.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-
-        var count=1;
-        for (var i = 83; i < 164; i++) {
-            tr = $('<tr/>');
-            tr.append("<td>" + count++ + "</td>");
-            tr.append("<td>" + JsonData[i].adi+ "</td>");
-            tr.append("<td>" + JsonData[i].yuzde + "</td>");
-            $('#myTable2').append(tr);
+    var yuzde = ((sdoz/83614362)*100).toFixed(1);
+    document.getElementById("genelOran").innerHTML = yuzde + "%";
+    document.getElementById('progress-value1').style.width = yuzde+ "%";
+    
+    var count=1;
+    for (var i = 83; i < 164; i++) {
+        tr = $('<tr/>');
+        tr.append("<td>" + count++ + "</td>");
+        tr.append("<td>" + JsonData[i].adi+ "</td>");
+        if(JsonData[i].yuzde > "% 75"){
+            tr.append("<td style='color:#4dc3fa'>" + JsonData[i].yuzde + "</td>");
+        }else{
+        tr.append("<td>" + JsonData[i].yuzde + "</td>");
         }
+        $('#myTable2').append(tr);
+    }
   
 }).catch(function (err) {
 	console.warn('Something went wrong.', err);
